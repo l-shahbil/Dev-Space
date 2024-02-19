2312d274559f5b98e1328a16cde10b4605d9f130
abud3l <abud99k@gmail.com>
2024-02-19 13:35:43 +0300

    add the project file

---
+++ DevSpace/DevSpace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevSpace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
